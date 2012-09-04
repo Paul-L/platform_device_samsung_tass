e4f7714764394e5797bb726011783243a0222f63
Marcin Chojnacki <marcinch7@gmail.com>
2012-09-04 16:26:01 +0200

    some changes
    Add bootlogo (works great, like on the stock rom) and remove max cpu
    frequency from ramdisk (it's already done by kernel)

--- device_gio.mk
+++ device_gio.mk
@@ -32,7 +32,8 @@ PRODUCT_PACKAGES += \
 ## Ramdisk
 PRODUCT_COPY_FILES += \
     device/samsung/gio/ramdisk/init.gt-s5660board.rc:root/init.gt-s5660board.rc \
-    device/samsung/gio/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc
+    device/samsung/gio/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc \
+    device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
 
 ## LDPI assets
 PRODUCT_AAPT_PREF_CONFIG := mdpi
