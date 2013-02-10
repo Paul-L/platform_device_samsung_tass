a635ef9bcf0b4ac808cc86e94b44e0a392ec7e81
Giovanni Santini <giovanni.santini@katamail.com>
2013-02-10 15:20:24 +0100

    Buildin camera is in MMB vendor

--- device_gio.mk
+++ device_gio.mk
@@ -41,8 +41,5 @@ PRODUCT_COPY_FILES += \
     device/samsung/gio/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc \
     device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
 
-#We do call Camera app because it won't be built instead
-PRODUCT_PACKAGES += Camera
-
 ## LDPI assets
 PRODUCT_AAPT_PREF_CONFIG := mdpi
