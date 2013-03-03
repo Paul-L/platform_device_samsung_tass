0ec95c19e013c166e2b455a331beb1b1d42b7a6d
mackief <davidepucci21@gmail.com>
2013-03-03 17:53:44 +0100

    Moved ramdisk in common device config

--- device_gio.mk
+++ device_gio.mk
@@ -36,9 +36,9 @@ PRODUCT_PACKAGES += \
 
 ## Ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/gio/ramdisk/init.gt-s5660board.rc:root/init.gt-s5660board.rc \
-    device/samsung/gio/ramdisk/init.gt-s5660board.usb.rc:root/init.gt-s5660board.usb.rc \
-    device/samsung/gio/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc \
+    device/samsung/msm7x27-common/ramdisk/init.device.rc:root/init.gt-s5660board.rc \
+    device/samsung/msm7x27-common/ramdisk/init.device.usb.rc:root/init.gt-s5660board.usb.rc \
+    device/samsung/msm7x27-common/ramdisk/ueventd.device.rc:root/ueventd.gt-s5660board.rc \
     device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
 
 ## LDPI assets
